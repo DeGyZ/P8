6805503fbb1507840c302d3a815985583ae821df
DeGyZ <119336329+DeGyZ@users.noreply.github.com>
2023-09-23 23:46:58 +0200

    Structure

--- modale.js
+++
@@ -1,9 +0,0 @@
-function afficherModal(projetId) {
-    var modal = document.getElementById(projetId + "-modal");
-    modal.style.display = "block";
-}
-
-function fermerModal(projetId) {
-    var modal = document.getElementById(projetId + "-modal");
-    modal.style.display = "none";
-}
